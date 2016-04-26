3e842545d85954a4b0838c6cd294b57e9db37739
Jesse Hughes <Jesse Hughes>
2016-04-25 19:36:54 -0500

    Added Waypoint.php and updated the namespacing for the remaining models.

--- laraveltripplanner/app/Group.php
+++ laraveltripplanner/app/Group.php
@@ -11,7 +11,7 @@ class Group extends Model
 
   //Relationships
   public function owner() {
-      return $this->belongsTo('App\User', 'user_id', 'id');
+      return $this->belongsTo('App\User');
   }
 
   public function members() {
--- laraveltripplanner/app/route.php
+++ laraveltripplanner/app/route.php
@@ -6,24 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Route extends Model
 {
-
     //Mass Assignment
     protected $fillable = array('rname', 'user_id');
 
     //Relationships
     public function owner() {
-        return $this->belongsTo('User', 'User');
+        return $this->belongsTo('App/User');
     }
 
     public function users() {
-        return $this->belongsToMany('User', 'route_access', 'routeid', 'userid');
+        return $this->belongsToMany('App/User', 'route_access', 'routeid', 'userid');
     }
 
     public function groups() {
-        return $this->belongsToMany('Group', 'group_route_access', 'routeid', 'groupid');
+        return $this->belongsToMany('App/Group', 'group_route_access', 'routeid', 'groupid');
     }
 
     public function waypoints() {
-        return $this->hasMany('Waypoint');
+        return $this->hasMany('App/Waypoint');
     }
 }
